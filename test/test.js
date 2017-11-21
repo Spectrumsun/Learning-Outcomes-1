f0f3abacb0837da14766366d6791a8d3e9454f77
Sectrumsun <taiwo.sunday@outlook.com>
2017-11-21 13:16:37 +0100

    oop'

--- test/test.js
+++ test/test.js
@@ -1,40 +1,66 @@
-import words from '../words'
+import MobilePhone from '../oop'
+import Tablet from '../tablet'
 import chai  from 'chai'
 
 const assert = chai.assert;
 
 
+describe("oop()", () => {
+	describe('Create an instance of a class', () => {
+        it('Create an instance of the MobilePhone class when instanciated', () => {
+            let samsung = new MobilePhone('Samsung');
+            let isTypeOf = samsung instanceof MobilePhone;
+            assert.equal(isTypeOf, true);
+        	});
+
+    describe('Create an instance of a class', () => {
+        it('Create an instance of the Tablet class when instanciated', () => {
+            let apple = new Tablet('apple');
+            let isTypeOf = apple instanceof Tablet;
+            assert.equal(isTypeOf, true);
+        	});
+    	});
+
+    describe('Create an instance of a class', () => {
+       	it('Should return OS is ANDROID Open Source', () => {
+            let samsung = new MobilePhone('samsung');
+			assert.equal(samsung.os(), "OS is ANDROID Open Source");
+        	});
+
+    	});
+
+    describe('Should be unable to make call when battery is below 35%', () => {
+       	it('Should return apple Battery too low to make calls', () => {
+            let apple = new MobilePhone('apple', 20);
+            assert.equal(apple.makeCall(), "apple Battery too low to make calls and recieve calls");
+        });
+
+    });
+
+    describe('Should be able to save new contact', () => {
+       	it('Should be able to save new contact', () => {
+            let nokia = new MobilePhone('nokia', 70, 200);
+            assert.equal(nokia.saveContact('jane', 'Doe', "0803985124"), "Contact Saved: First Name:  jane  Last Name:  Doe  Phome Number: 0803985124");
+        });
+
+    });
+
+    describe('Should not be able to save new contact', () => {
+       	it('Should be able to save new contact on apple tablet', () => {
+            let apple = new Tablet('apple', 70, 200);
+            assert.equal(apple.makeCall(), 'apple table cant send application');
+        });
+
+    });
 
 
-
-describe("words()", () => {
-	it("counting a single word", () => {
-    let Count = { word: 1 };
-    assert.equal(JSON.stringify(words("word")), JSON.stringify(Count));
-  });
-
-	it("count double words", () => {
-    let Count = { olly: 1, olly: 1 };
-    assert.equal(JSON.stringify(words("olly")), JSON.stringify(Count));
-  });
-
-	it("counts multiple occurrences of words", () => {
-	  let Count =  { my: 5, is: 2, a: 1, house: 3, }
-	 assert.equal(JSON.stringify(words("my my my is a my my house is house house")), JSON.stringify(Count));
-   });
-
-	it("check for empty space", () => {
-	  let Count =  { olly: 1,    };
-	 assert.equal(JSON.stringify(words("olly")), JSON.stringify(Count));
-   });
-
-	it("it counts both capital and small letters has same word", () => {
-	  let Count =  "empty string not allowed"
-	 assert.equal(JSON.stringify(words("")), JSON.stringify(Count));
-   });
-
+	});
 
 });
 
-words("")
 
+
+
+      
+
+ 
